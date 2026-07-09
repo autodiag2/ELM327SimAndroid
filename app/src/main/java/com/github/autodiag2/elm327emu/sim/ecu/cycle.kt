9d22d90559e538d186485e8dd8576fd265efe1b3
autodiag2 <autodiag@netcourrier.com>
2026-07-09 10:56:31 +0200

    enhance bytes targeting with signal understanding

--- app/src/main/java/com/github/autodiag2/elm327emu/sim/ecu/cycle.kt
+++ app/src/main/java/com/github/autodiag2/elm327emu/sim/ecu/cycle.kt
@@ -8,6 +8,7 @@ import com.github.autodiag2.elm327emu.R
 import com.github.autodiag2.elm327emu.libautodiag
 import org.json.JSONArray
 import org.json.JSONObject
+import androidx.appcompat.widget.SwitchCompat
 
 class EcuCycle(
     address: EcuAddress = DEFAULT_ADDRESS,
@@ -16,24 +17,28 @@ class EcuCycle(
 ): Ecu(EcuType.cycle, address, name ?: activity.getString(EcuType.cycle.label_id), activity) {
 
     private val gears: EditText
+    private val use_signals: SwitchCompat
 
     init {
         LayoutInflater.from(activity).inflate(R.layout.sim_ecu_cycle, this, true)
         gears = findViewById(R.id.sim_ecu_cycle_gears_input)
+        use_signals = findViewById(R.id.sim_ecu_cycle_use_signals)
         findViewById<Button>(R.id.sim_ecu_cycle_gears_validate).setOnClickListener {
             libautodiag.simEcuLoadFromJson(address.toByte(), toJson().toString())
         }
-        libautodiag.setResponseTypeByAddress(address, "cycle")
+        libautodiag.setResponseTypeByAddress(address, EcuType.cycle.name)
     }
 
     override fun toJsonInternal(): JSONObject {
         val content = JSONObject()
         content.put("gears", gears.getText().toString().toInt())
+        content.put("use_signals", use_signals.isChecked)
         return content
     }
 
     override fun fromJsonInternal(content: JSONObject) {
         gears.setText(content.getInt("gears").toString())
+        use_signals.isChecked = content.getBoolean("use_signals")
     }
 
 }
--- app/src/main/java/com/github/autodiag2/elm327emu/sim/ecu/random.kt
+++ app/src/main/java/com/github/autodiag2/elm327emu/sim/ecu/random.kt
@@ -8,6 +8,7 @@ import com.github.autodiag2.elm327emu.R
 import com.github.autodiag2.elm327emu.libautodiag
 import org.json.JSONArray
 import org.json.JSONObject
+import androidx.appcompat.widget.SwitchCompat
 
 class EcuRandom(
     address: EcuAddress = DEFAULT_ADDRESS,
@@ -16,24 +17,28 @@ class EcuRandom(
 ): Ecu(EcuType.random, address, name ?: activity.getString(EcuType.random.label_id), activity) {
 
     private val seed: EditText
+    private val use_signals: SwitchCompat
 
     init {
         LayoutInflater.from(activity).inflate(R.layout.sim_ecu_random, this, true)
         seed = findViewById(R.id.sim_ecu_random_seed_input)
+        use_signals = findViewById(R.id.sim_ecu_random_use_signals)
         findViewById<Button>(R.id.sim_ecu_random_validate).setOnClickListener {
             libautodiag.simEcuLoadFromJson(address.toByte(), toJson().toString())
         }
-        libautodiag.setResponseTypeByAddress(address, "random")
+        libautodiag.setResponseTypeByAddress(address, EcuType.random.name)
     }
 
     override fun toJsonInternal(): JSONObject {
         val content = JSONObject()
         content.put("seed", seed.getText().toString().toInt())
+        content.put("use_signals", use_signals.isChecked)
         return content
     }
 
     override fun fromJsonInternal(content: JSONObject) {
         seed.setText(content.getInt("seed").toString())
+        use_signals.isChecked = content.getBoolean("use_signals")
     }
 
 }
