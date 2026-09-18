f14c5a5177065f640d3e8dacc3a11076e14ac2fe
autodiag2 <autodiag@netcourrier.com>
2026-09-18 11:31:06 +0200

    add an unsupported popup for lite only of the godot feature

--- app/src/lite/java/com/github/autodiag2/elm327emu/sim/ecu_gui_godot.kt
+++ app/src/lite/java/com/github/autodiag2/elm327emu/sim/ecu_gui_godot.kt
@@ -1,12 +1,59 @@
 package com.github.autodiag2.elm327emu.sim
 
+import android.app.AlertDialog
+import android.content.Intent
+import android.net.Uri
+import android.text.SpannableStringBuilder
+import android.text.Spanned
+import android.text.style.URLSpan
+import android.text.method.LinkMovementMethod
+import android.widget.TextView
+import androidx.core.view.setPadding
 import com.github.autodiag2.elm327emu.MainActivity
+import com.github.autodiag2.elm327emu.R
 
 class GuiGodot(
     @Suppress("UNUSED_PARAMETER")
     private val activity: MainActivity
 ) {
+
     fun show() {
+        val releasesUrl =
+            "https://github.com/autodiag2/ELM327SimAndroid/releases/latest"
+
+        val message = TextView(activity).apply {
+            setPadding(
+                24,
+                8,
+                24,
+                8
+            )
+
+            val text = SpannableStringBuilder()
+
+            text.append(activity.getString(R.string.sim_ecu_gui_godot_lite_unsuported_dialog_desc))
+
+            val releasesStart = text.length
+            text.append(releasesUrl)
+            val releasesEnd = text.length
+
+            text.setSpan(
+                URLSpan(releasesUrl),
+                releasesStart,
+                releasesEnd,
+                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+            )
+
+            this.text = text
+            movementMethod = LinkMovementMethod.getInstance()
+            setTextIsSelectable(true)
+        }
+
+        AlertDialog.Builder(activity)
+            .setTitle(activity.getString(R.string.sim_ecu_gui_godot_lite_unsuported_dialog_title))
+            .setView(message)
+            .setPositiveButton(android.R.string.ok, null)
+            .show()
     }
 
     fun refreshPeriodically(
