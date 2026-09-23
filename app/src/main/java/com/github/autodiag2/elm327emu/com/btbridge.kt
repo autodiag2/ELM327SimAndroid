f3b5774d9f14f633de6993b6c676d0ecad51ae02
autodiag2 <autodiag@netcourrier.com>
2026-09-23 16:40:19 +0200

    Bluetooth Classic: listen both on the secure and the insecure channel even if most scantool expect a paired device

--- app/src/main/java/com/github/autodiag2/elm327emu/com/btbridge.kt
+++ app/src/main/java/com/github/autodiag2/elm327emu/com/btbridge.kt
@@ -11,6 +11,9 @@ import com.github.autodiag2.elm327emu.LogLevel
 import com.github.autodiag2.elm327emu.MainActivity
 import kotlinx.coroutines.channels.Channel
 import com.github.autodiag2.elm327emu.sim.EmuInterface
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.selects.select
 
 class BluetoothBridge(
     emu: EmuInterface,
@@ -22,93 +25,158 @@ class BluetoothBridge(
     private val classicalBtUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
     private val requestQueue = Channel<ByteArray>(Channel.UNLIMITED)
 
-    private var server: BluetoothServerSocket? = null
+    private var secureServer: BluetoothServerSocket? = null
+    private var insecureServer: BluetoothServerSocket? = null
     private var socket: BluetoothSocket? = null
     private var bt_input: InputStream? = null
     private var bt_output: OutputStream? = null
 
     override suspend fun accept() {
-        var clientIdentifier = ""
+        appendLog(
+            getString(R.string.log_bt_waiting_for_connection),
+            LogLevel.INFO
+        )
+
         try {
+            coroutineScope {
+                val secureAccept = async(Dispatchers.IO) {
+                    secureServer?.accept()
+                }
 
-            appendLog(getString(R.string.log_bt_waiting_for_connection), LogLevel.INFO)
+                val insecureAccept = async(Dispatchers.IO) {
+                    insecureServer?.accept()
+                }
 
-            socket = server?.accept()
-            appendLog(getString(R.string.log_bt_client_connected, socket?.remoteDevice?.address),
-                LogLevel.INFO
-            )
-            clientIdentifier = socket!!.remoteDevice!!.address
-            listener?.onClientConnect(clientIdentifier, this)
+                val acceptedSocket = select<BluetoothSocket?> {
+                    secureAccept.onAwait { socket ->
+                        insecureAccept.cancel()
+                        socket
+                    }
 
-            bt_input = socket?.inputStream
-            bt_output = socket?.outputStream
-
-            val reader = scope.launch {
-                while (isActive) {
-                    val bufferBT = ByteArray(1024)
-                    try {
-                        val n = bt_input?.read(bufferBT) ?: break
-                        if (n <= 0) break
-
-                        requestQueue.send(bufferBT.copyOf(n))
-
-                    } catch (e: Exception) {
-                        appendLog(
-                            getString(R.string.log_bt_btToLoop_failed, e.message),
-                            LogLevel.DEBUG
-                        )
-                        break
+                    insecureAccept.onAwait { socket ->
+                        secureAccept.cancel()
+                        socket
                     }
                 }
-            }
 
-            val worker = scope.launch {
-                val bufferLoop = ByteArray(1024)
+                if (acceptedSocket == null) {
+                    return@coroutineScope
+                }
 
-                while (isActive) {
-                    try {
-                        val request = requestQueue.receive()
+                socket = acceptedSocket
 
-                        activity.onDataReceived(request, request.size)
+                if (socket == null) {
+                    return@coroutineScope
+                }
 
-                        val n = emu.transact(
-                            request,
-                            request.size,
-                            bufferLoop
-                        )
-                        if (n <= 0) break
+                var clientIdentifier = ""
 
-                        bt_output?.write(bufferLoop, 0, n)
-                        bt_output?.flush()
+                try {
+                    clientIdentifier = acceptedSocket.remoteDevice.address
 
-                        activity.onDataSent(bufferLoop, n)
+                    appendLog(
+                        getString(
+                            R.string.log_bt_client_connected,
+                            clientIdentifier
+                        ),
+                        LogLevel.INFO
+                    )
 
-                    } catch (e: Exception) {
-                        appendLog(
-                            getString(R.string.log_bt_loopToBt_failed, e.message),
-                            LogLevel.DEBUG
-                        )
-                        break
+                    listener?.onClientConnect(clientIdentifier, this@BluetoothBridge)
+
+                    val input = acceptedSocket.inputStream
+                    val output = acceptedSocket.outputStream
+
+                    bt_input = input
+                    bt_output = output
+
+                    val reader = launch(Dispatchers.IO) {
+                        try {
+                            while (isActive) {
+                                val bufferBT = ByteArray(1024)
+                                val n = input.read(bufferBT)
+
+                                if (n <= 0) break
+
+                                requestQueue.send(bufferBT.copyOf(n))
+                            }
+                        } catch (e: Exception) {
+                            appendLog(
+                                getString(
+                                    R.string.log_bt_btToLoop_failed,
+                                    e.message
+                                ),
+                                LogLevel.DEBUG
+                            )
+                        }
                     }
+
+                    val worker = launch(Dispatchers.IO) {
+                        val bufferLoop = ByteArray(1024)
+
+                        try {
+                            while (isActive) {
+                                val request = requestQueue.receive()
+
+                                activity.onDataReceived(request, request.size)
+
+                                val n = emu.transact(
+                                    request,
+                                    request.size,
+                                    bufferLoop
+                                )
+
+                                if (n <= 0) break
+
+                                output.write(bufferLoop, 0, n)
+                                output.flush()
+
+                                activity.onDataSent(bufferLoop, n)
+                            }
+                        } catch (e: Exception) {
+                            appendLog(
+                                getString(
+                                    R.string.log_bt_loopToBt_failed,
+                                    e.message
+                                ),
+                                LogLevel.DEBUG
+                            )
+                        }
+                    }
+
+                    reader.join()
+                    worker.cancelAndJoin()
+
+                } finally {
+                    listener?.onClientDisconnect(clientIdentifier, this@BluetoothBridge)
+
+                    try {
+                        acceptedSocket.close()
+                    } catch (_: Exception) {
+                    }
+
+                    bt_input?.close()
+                    bt_output?.close()
+                    bt_input = null
+                    bt_output = null
+                    socket = null
+                    appendLog(
+                        getString(R.string.log_bt_connection_closed),
+                        LogLevel.INFO
+                    )
                 }
             }
-
-            reader.join()
-            worker.cancelAndJoin()
         } catch (e: CancellationException) {
-            appendLog(getString(R.string.log_bt_cancelled), LogLevel.DEBUG)
+            appendLog(
+                getString(R.string.log_bt_cancelled),
+                LogLevel.DEBUG
+            )
             throw e
         } catch (e: Exception) {
-            appendLog(getString(R.string.log_bt_error, e.message), LogLevel.DEBUG)
-        } finally {
-            listener?.onClientDisconnect(clientIdentifier, this)
-            bt_input?.close()
-            bt_output?.close()
-            socket?.close()
-            bt_input = null
-            bt_output = null
-            socket = null
-            appendLog(getString(R.string.log_bt_connection_closed), LogLevel.INFO)
+            appendLog(
+                getString(R.string.log_bt_error, e.message),
+                LogLevel.DEBUG
+            )
         }
     }
 
@@ -117,7 +185,17 @@ class BluetoothBridge(
             activity.showBluetoothEnablePopup()
             return
         }
-        server = activity.btAdapter.listenUsingInsecureRfcommWithServiceRecord(getString(R.string.app_name), classicalBtUUID)
+        secureServer =
+            activity.btAdapter.listenUsingRfcommWithServiceRecord(
+                getString(R.string.app_name),
+                classicalBtUUID
+            )
+
+        insecureServer =
+            activity.btAdapter.listenUsingInsecureRfcommWithServiceRecord(
+                getString(R.string.app_name),
+                classicalBtUUID
+            )
     }
 
     override fun stop() {
@@ -131,8 +209,11 @@ class BluetoothBridge(
             socket?.close()
             socket = null
 
-            server?.close()
-            server = null
+            insecureServer?.close()
+            insecureServer = null
+
+            secureServer?.close()
+            secureServer = null
         } catch (_: Exception) {
             
         }
